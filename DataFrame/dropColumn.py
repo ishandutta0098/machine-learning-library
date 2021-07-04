edd8e0caeedd8376c1592619516250067865ed64
Ishan Dutta <ishandutta0098@gmail.com>
2021-07-04 15:52:12 +0530

    Delete DataFrame directory

--- DataFrame/dropColumn.py
+++
@@ -1,2 +0,0 @@
-#Drop Selected Columns from DataFrame
-df.drop(columns = 'column1', inplace = True)
--- DataFrame/renameColumns.py
+++
@@ -1,5 +0,0 @@
-#Rename Selected Columns
-df.rename(columns = {'old1':'new1', 'old2':'new2'}, inplace = True)
-
-#Rename all columns
-df.columns = ['new1', 'new2', 'new3']
